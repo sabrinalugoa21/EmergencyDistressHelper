d32b5ba1c0ea37842ab325af719fb361c5ecf319
sabrina lugo <sabrinalugo@sabrinas-MacBook-Air-2.local>
2022-03-22 22:21:23 -0700

    updated SignUp backend and User class

--- app/src/main/java/com/example/emergencydistresshelper/SignUp.java
+++ app/src/main/java/com/example/emergencydistresshelper/SignUp.java
@@ -7,56 +7,62 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SignUp {
+public class SignUp
+{
     //should have a person data type
     //need to make a person class
-    public void getSignUp(User client)
+    public Boolean getSignUp(User client)
     {
-        User customer = new User();
+        //User customer = new User();
         Connection connection;
-        String url = "jdbc:mysql://localhost:3306/javabase";
+        String url = "jdbc:mysql://localhost:3306/EDH";
         String username = "root";
-        String password = "toor";
+        String password = "";
+        Boolean status = false;
 
         System.out.println("Connecting database..");
 
-
-        try
-        {
+        try {
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Database connected!");
 
             //Statement findUser = connection.createStatement();
-            PreparedStatement query = connection.prepareStatement("SELECT COUNT(*) FROM Users WHERE email = ?");
-            query.setString(1, client.Email);
+            connection.setAutoCommit(false);
+            PreparedStatement query = connection.prepareStatement("SELECT COUNT(*) FROM Users WHERE FirstName = ? AND LastName = ?" +
+                    "AND Email = ?");
+            query.setString(1, client.FirstName);
+            query.setString(2, client.LastName);
+            query.setString(3, client.Email);
             ResultSet rs = query.executeQuery();
             rs.next();
 
-            try
-            {
+            try {
                 if (rs.getInt(1) == 0) {
-                    query = connection.prepareStatement("INSERT INTO User (FirstN, LastN, PhoneNum, Email, Address)" +
-                                                        "VALUES(?, ?, ?, ?)");
+                    query = connection.prepareStatement("INSERT INTO Users (FirstName, LastName, PhoneNumber, Email, " +
+                            "Street, City, State, ZipCode) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
                     query.setString(1, client.FirstName);
                     query.setString(2, client.LastName);
                     query.setString(3, client.PhoneNumber);
                     query.setString(4, client.Email);
-                    query.setString(5, client.Address);
-                    query.executeQuery();
+                    query.setString(5, client.Street);
+                    query.setString(6, client.City);
+                    query.setString(7, client.State);
+                    query.setString(8, client.ZipCode);
+                    query.executeUpdate();
                     connection.commit();
+                    connection.close();
+                    status = true;
+
+                } else {
+                    status = false;
                 }
-            }
-            catch (SQLException se)
-            {
+            } catch (SQLException se) {
                 connection.rollback();
             }
 
-        }
-        catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             throw new IllegalStateException("Cannot connect the database!", ex);
         }
-
+        return status;
     }
-
 }
--- app/src/main/java/com/example/emergencydistresshelper/User.java
+++ app/src/main/java/com/example/emergencydistresshelper/User.java
@@ -1,21 +1,28 @@
 package com.example.emergencydistresshelper;
 import java.lang.String;
-public class User {
 
+public class User {
     //User information
     public String FirstName;
     public String LastName;
     public String PhoneNumber;
     public String Email;
     public String Address;
+    public String Street;
+    public String City;
+    public String State;
+    public String ZipCode;
 
-    public User(String First, String Last, String phone, String em, String add)
+    public User(String First, String Last, String phone, String em, String str, String cit, String stat, String zip)
     {
         FirstName = First;
         LastName = Last;
         PhoneNumber = phone;
         Email = em;
-        Address = add;
+        Street = str;
+        City = cit;
+        State = stat;
+        ZipCode = zip;
 
     }
 
@@ -25,14 +32,21 @@ public class User {
         LastName = "";
         PhoneNumber = "";
         Email = "";
-        Address = "";
+        Street = "";
+        City = "";
+        State = "";
+        ZipCode = "";
+
     }
     //Setting functions
-    public void setFirstName(String First){ FirstName = FirstName; }
+    public void setFirstName(String First){ FirstName = First; }
     public void setLastName(String Last){LastName = Last; }
     public void setPhoneNumber(String phone){PhoneNumber = phone;}
     public void setEmail(String em) {Email = em; }
-    public void setAddress(String add) {Address = add; }
+    public void setStreet(String str) {Street = str; }
+    public void setCity(String cit) {City = cit; }
+    public void setState(String stat) {State = stat; }
+    public void setZipCode(String zip) {ZipCode = zip; }
 
     //Getting functions
     public String getFirstName() {return FirstName; }
@@ -40,5 +54,9 @@ public class User {
     public String getPhoneNumber(){return PhoneNumber; }
     public String getEmail(){return Email; }
     public String getAddress(){return Address; }
-
+    public String getStreet() {return Street; }
+    public String getCity() {return City; }
+    public String getState() {return State; }
+    public String getZipCode() { return ZipCode; }
 }
+
