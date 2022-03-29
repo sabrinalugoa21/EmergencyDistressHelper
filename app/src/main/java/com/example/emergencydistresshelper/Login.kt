821f9abcbc3091da907b4dacaf9a7707597125dd
Jaykkwak <63373759+Jaykkwak@users.noreply.github.com>
2022-03-29 13:30:39 -0700

    Connect login and register page

--- app/src/main/java/com/example/emergencydistresshelper/Login.kt
+++ app/src/main/java/com/example/emergencydistresshelper/Login.kt
@@ -1,11 +1,20 @@
 package com.example.emergencydistresshelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginToreg = findViewById<Button>(R.id.loginToreg)
+
+        loginToreg.setOnClickListener  {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
