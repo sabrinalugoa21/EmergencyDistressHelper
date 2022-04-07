6805b9a2c1ed935396b87f1309b8902e8f4637ab
Manny-S <32533964+Manny-S@users.noreply.github.com>
2022-04-07 16:39:49 -0700

    Twilio SMS working
    Can send text messages with the SOS button

--- app/src/main/java/com/example/emergencydistresshelper/Homepage.kt
+++ app/src/main/java/com/example/emergencydistresshelper/Homepage.kt
@@ -1,6 +1,8 @@
 package com.example.emergencydistresshelper
 
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -13,10 +15,15 @@ class Homepage : AppCompatActivity() {
 
         val sosButton = findViewById<Button>(R.id.sos_button)
 
+        //needed for okhttp for Twilio Text Message
+        StrictMode.setThreadPolicy(ThreadPolicy.Builder().permitAll().build())
+
         sosButton.setOnLongClickListener {
             val sosMessage = "Sending Text Message"
             val sosSnackbar = Snackbar.make(sosButton, sosMessage, Snackbar.LENGTH_SHORT)
             sosSnackbar.show()
+
+            TextMessage.sendTextMessage()
             return@setOnLongClickListener true
         }
     }
---
+++ app/src/main/java/com/example/emergencydistresshelper/TextMessage.java
@@ -0,0 +1,43 @@
+package com.example.emergencydistresshelper;
+
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.*;
+
+public class TextMessage {
+    private static final String ACCOUNT_SID = "ACd51283a55fad386b1cadeb8e395fffff";
+    private static final String AUTH_TOKEN = "";
+    private static final String fromNumber = "+19205192974";
+
+    public static void sendTextMessage() {
+        //get number using getPhoneNumber() from Contact.java
+        String toNumber = "";
+        String messageBody = "EDH - Twilio SOS Button";
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.twilio.com/2010-04-01/Accounts/" + ACCOUNT_SID + "/SMS/Messages";
+        String base64EncodedCredentials = "Basic " +
+                Base64.encodeToString((ACCOUNT_SID + ":" + AUTH_TOKEN).getBytes(), Base64.NO_WRAP);
+
+        RequestBody body = new FormBody.Builder()
+                .add("From", fromNumber)
+                .add("To", toNumber)
+                .add("Body", messageBody)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .header("Authorization", base64EncodedCredentials)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Log.d("TAG", "sendTextMessage: " + response.body().string());
+        }
+        catch (IOException e) { e.printStackTrace();}
+    }
+}
