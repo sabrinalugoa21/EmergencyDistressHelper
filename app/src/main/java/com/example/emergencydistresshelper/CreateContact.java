893ff9c5a838ec928788383f1f520b34683da51e
Gregory Chen <gregchen56@gmail.com>
2022-05-02 19:09:32 -0700

    Fixed bug where creating contacts was overwriting existing contacts

--- app/src/main/java/com/example/emergencydistresshelper/CreateContact.java
+++ app/src/main/java/com/example/emergencydistresshelper/CreateContact.java
@@ -2,17 +2,24 @@ package com.example.emergencydistresshelper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import java.util.regex.Pattern;
 
 public class CreateContact extends AppCompatActivity implements View.OnClickListener {
@@ -21,7 +28,7 @@ public class CreateContact extends AppCompatActivity implements View.OnClickList
     private EditText editTextFullName, editTextPhoneNumber, editTextMessage;
 
     private FirebaseUser user;
-    private DatabaseReference mDatabase;
+    private DatabaseReference dbReference;
 
 
     @Override
@@ -30,7 +37,10 @@ public class CreateContact extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_create_contact);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        dbReference = FirebaseDatabase
+                .getInstance()
+                .getReference("Users")
+                .child(user.getUid());
 
         cancel = (Button) findViewById(R.id.btn_cancel_contact);
         cancel.setOnClickListener(this);
@@ -110,38 +120,46 @@ public class CreateContact extends AppCompatActivity implements View.OnClickList
         // Create new contact object and reference the
         // user's contacts in the database
         Contact contact = new Contact(fullName, phone, message);
-        DatabaseReference userContactRef = mDatabase
-                .child("Users")
-                .child(user.getUid())
-                .child("Contacts");
 
-        // Get number of contacts the user already has
-        Task<DataSnapshot> numOfContactsTask = userContactRef.get();
-        while (!numOfContactsTask.isComplete());
-        long numOfContacts = numOfContactsTask.getResult().getChildrenCount();
+        // Get the highest index of an existing contact
+        Query latestContactQuery = dbReference.child("Contacts").orderByKey().limitToLast(1);
+        latestContactQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int latestContactIndex = -1;
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    latestContactIndex = Integer.parseInt(ds.getKey());
+                }
 
+                // If the user is adding a contact for the first time,
+                // it must be the default contact
+                if (snapshot.getChildrenCount() == 0) {
+                    dbReference.child("defaultContactIndex").setValue("0");
+                }
 
-        // If the user is adding a contact for the first time,
-        // it must be the default contact
-        DatabaseReference userRef = mDatabase
-                .child("Users")
-                .child(user.getUid());
+                // Add new contact for user in the database
+                dbReference.child("Contacts").child(String.valueOf(latestContactIndex + 1)).setValue(contact, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                        if (error != null) {
+                            Toast.makeText(CreateContact.this, "Failed to add the contact.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(CreateContact.this, "Successfully added contact!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        if (numOfContacts == 0) {
-            userRef
-                    .child("defaultContactIndex")
-                    .setValue("0");
-        }
+                // only update if first contact
+                if (snapshot.getChildrenCount() == 0) {
+                    TextMessage.update_number_and_message();
+                }
+            }
 
-        // Add new contact for user in the database
-        userContactRef
-                .child(String.valueOf(numOfContacts))
-                .setValue(contact);
-
-        //only update if first contact
-        if (numOfContacts == 0) {
-            TextMessage.update_number_and_message();
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Log", "Failed to retrieve snapshot for user contact list");
+            }
+        });
     }
 
 }
